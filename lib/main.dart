ba9d2480b590db8840fd074533aa4a2ef2824ce3
Egor <mail@nebesa.me>
2018-11-17 13:11:45 +0300

    second

--- lib/main.dart
+++ lib/main.dart
@@ -1,36 +1,30 @@
+// Copyright 2018 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+// You can read about packages here: https://flutter.io/using-packages/
 import 'package:flutter/material.dart';
 
-void main(List<String> args) {
-  runApp(
-    MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text("Hello rectangle"),
-        ),
-        body: HelloRectangle(),
-      ),
-    ),
-  );
+// You can use a relative import, i.e. `import 'category_route.dart;'` or
+// a package import, as shown below.
+// More details at http://dart-lang.github.io/linter/lints/avoid_relative_lib_imports.html
+import 'package:flutterjam/category_route.dart';
+
+/// The function that is called when main.dart is run.
+void main() {
+  runApp(UnitConverterApp());
 }
 
-class HelloRectangle extends StatelessWidget {
+/// This widget is the root of our application.
+///
+/// The first screen we see is a list [Categories].
+class UnitConverterApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Container(
-        padding: EdgeInsets.all(16.0),
-        color: Colors.greenAccent,
-        height: 400.0,
-        width: 300.0,
-        child: Center(
-          child: Text(
-            'Hello rectangle',
-            style: TextStyle(fontSize: 20.0),
-            textAlign: TextAlign.center,
-          ),
-        ),
-      ),
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Unit Converter',
+      home: CategoryRoute(),
     );
   }
 }
